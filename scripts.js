dbddac4617ba3abe851b24339782438f1487436f
Onandi Skeen <onandiskeen@gmail.com>
2021-10-23 06:29:04 -0500

    Edited scrips.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 window.onload = function(){
-    console.log("yes");
     let button = document.querySelector('button');
+    let msg = document.querySelector('.message');
+    const emailreg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
-    console.log(button);
 
     button.addEventListener('click', function pressed(e){
         e.preventDefault();
+        let email = document.querySelector('#email').value;
+        if(emailreg.test(email)){
+            msg.innerHTML = 'Thank you! Your email address ' +email+ ' has been added to the mailing list!';
+        }else {
+            msg.innerHTML = 'Please enter a valid email address';
+        }
+        
     });
 
 }
