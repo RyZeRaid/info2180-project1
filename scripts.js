f838c5659640ef11c646c5d5261783e6d94e5846
Onandi Skeen <onandiskeen@gmail.com>
2021-10-23 05:48:14 -0500

    Edited styles.css and scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+    console.log("yes");
+    let button = document.querySelector('button');
+
+    console.log(button);
+
+    button.addEventListener('click', function pressed(e){
+        e.preventDefault();
+    });
+
+}
